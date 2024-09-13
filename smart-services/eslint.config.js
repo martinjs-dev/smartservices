8b837b834a5244ed88d189d8e602cf03b7239630
martinjs-dev <martindohou.pro@gmail.com>
2024-09-13 22:02:38 +0100

    [MERGE] : stagin/feat-crud-users

--- smart-services/eslint.config.js
+++ smart-services/eslint.config.js
@@ -3,7 +3,7 @@ import globals from "globals";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-const unusedImports = require('eslint-plugin-unused-imports');
+import unusedImports from 'eslint-plugin-unused-imports'
 
 export default [
   { ignores: ["dist"] },
